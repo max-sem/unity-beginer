54f1480770eb9d5a7901d1f3c01661f0c5a12bf5
max-sem <max_sem@mail.ru>
2023-03-04 12:43:30 +0300

    Коллайдеры и столконовения. Промежуточное сохранение

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -11,6 +11,18 @@ public class PlayerBehavior : MonoBehaviour
     private float vInput;
     private float hInput;
 
+    // 7 
+    // содержит информацию о компоненте Rigidbody капсулы
+    private Rigidbody _rb;
+
+    // 8
+    void Start()
+    {
+        // 9  Получаем компонент объекта по типу 
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
     void Update()
     {
         // 3 Ловим изменение вертикальной оси (-1 0 1) и умножаем на скорость
@@ -20,8 +32,9 @@ public class PlayerBehavior : MonoBehaviour
         // 4 Ловим горизонтальное ВРАЩЕНИЕ !!! (-1 0 1) и умножаем на скорость
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        Debug.Log($"vInput: {vInput}, hInput: {hInput}");
 
+        /* 10 Закомментировали вызовы Transform и Rotate с прошлого урока
+         
         // 5 Translate перемещает относительно ориентации !(относительно своей локальной оси Z)
         //  Vector3.forward = Vector3(0, 0, 1)	- норммализованный вектор по Z
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
@@ -30,5 +43,26 @@ public class PlayerBehavior : MonoBehaviour
         // Vector3.up = Vector3(0, 1, 0) Вращение происходит ВОКРУГ указанной оси
         // Был бы Translate для Vector3.up, то прыгнул бы
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */
     }
+
+
+    private void FixedUpdate() // 11 Специальный Update для физики
+    {
+        // 12 Переменная для хранения вращения 
+        Vector3 rotation = Vector3.up * hInput;
+
+        // 13 На вход - Vector3, на выход - значение поворота в углах Эйлера
+        // Преобразование в тип вращения, который нужен для MoveRotation
+        // fixedDeltaTime - аналогичная бодяга deltaTime
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        
+        // 14
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+
+        // 5
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+    }
+
 }
