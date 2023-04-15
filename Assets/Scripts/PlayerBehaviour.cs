32d67885bde6a691b53d6a0fee02b11ebf961ecc
max-sem <max_sem@mail.ru>
2023-04-15 18:53:14 +0300

    Прыжки доделанные. Запрет прыжка с толчком от воздуха

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,14 +9,22 @@ public class PlayerBehaviour : MonoBehaviour
 
     public float jumpVelocity = 10f;
 
+    public float distanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+
     private float vInput;
     private float hInput;
-
     private Rigidbody _rb;
+
+    private CapsuleCollider _col;
+
+  
     private void Start()
     {
-        _rb = this.GetComponent<Rigidbody>();
+        _rb  = this.GetComponent<Rigidbody>();
+        _col = this.GetComponent<CapsuleCollider>();
     }
+
     void Update()
     {
         vInput = Input.GetAxis("Vertical") * moveSpeed;
@@ -25,26 +33,26 @@ public class PlayerBehaviour : MonoBehaviour
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
         //this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Debug.Log(jumpVelocity);
+        if (IsGrounded() && (Input.GetKeyDown(KeyCode.Q)))
+        {            
             _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
-
         }
+                
 
     }
     private void FixedUpdate()
     {        
-        
-        
-        
-        
         Vector3 rotation = Vector3.up * hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position  + this.transform.forward * vInput * Time.fixedDeltaTime);
-        _rb.MoveRotation(_rb.rotation * angleRot);
-        
+        _rb.MoveRotation(_rb.rotation * angleRot);        
     }
 
+    private bool IsGrounded() 
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z );
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
+    }
 
 }
