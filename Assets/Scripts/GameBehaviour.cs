57088471385c6ad2d4326f2ddcc50df93d65cc54
max-sem <max_sem@mail.ru>
2023-05-13 13:52:41 +0300

    Игровой менеджер. get и set для свойства

---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour
+{
+    private int _itemCollected = 0; 
+
+    public int Items 
+    {
+        get 
+        {
+            return _itemCollected;
+        }
+        set 
+        {
+            _itemCollected = value;
+            Debug.LogFormat("Собрано предметов: {0}", _itemCollected);
+
+        }    
+    }
+     
+    private int _playerHP = 10;
+    public int HP
+    {
+        get 
+        { 
+            return _playerHP; 
+        }
+        set
+        {
+            _playerHP = value;
+            Debug.LogFormat("Осталось жизней: {0}, ", _playerHP);
+        }    
+    }    
+}
--- Assets/Scripts/ItemBehavior.cs
+++ Assets/Scripts/ItemBehavior.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehaviour gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehaviour>();
+
+
+    }
+
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player") 
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Здоровье получено!");
+
+            gameManager.Items += 1;
+
         }
-                
-    
-    
+
+       
+
+
+
     }
     
 
