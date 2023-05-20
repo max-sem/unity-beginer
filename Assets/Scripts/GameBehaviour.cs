5cb6e633cf17c6a423cacc1a54735fc43c83aef4
max-sem <max_sem@mail.ru>
2023-05-20 17:02:25 +0300

    GUI. Панель с информацией

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class GameBehaviour : MonoBehaviour
 {
+    public string labelText = "Собери все 4 предмета и получи свободу!";
+    public int maxItems = 4; 
+    
+    
     private int _itemCollected = 0; 
-
     public int Items 
     {
         get 
@@ -16,10 +19,17 @@ public class GameBehaviour : MonoBehaviour
         {
             _itemCollected = value;
             Debug.LogFormat("Собрано предметов: {0}", _itemCollected);
-
+            if (_itemCollected >= maxItems)
+            {
+                labelText = " Ты нашел все предметы!";
+            }
+            else
+            {
+                labelText = "Предмет найден. Найди еще " + (maxItems - _itemCollected) + " предметов!";
+            
+            }
         }    
-    }
-     
+    }     
     private int _playerHP = 10;
     public int HP
     {
@@ -32,5 +42,15 @@ public class GameBehaviour : MonoBehaviour
             _playerHP = value;
             Debug.LogFormat("Осталось жизней: {0}, ", _playerHP);
         }    
-    }    
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Жизнь: " + _playerHP);
+        GUI.Box(new Rect(20, 50, 150, 25), "Предметы: " + _itemCollected);
+        GUI.Label( new Rect(20 , 80, 300, 50), labelText);
+    }
+
+        
+
 }
